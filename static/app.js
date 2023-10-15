ab31a35547f0694b117941248993bb633bc1fda0
Dmitrii Vlasov <vlasdv@users.noreply.github.com>
2023-10-15 21:43:37 +0530

    Add function for playing a single round

--- static/app.js
+++ static/app.js
@@ -1,7 +1,11 @@
 const possibleChoices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-  return possibleChoices[Math.floor(Math.random() * 3)];
+  let computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
+  console.log("computer: " + computerChoice);
+  return computerChoice;
 }
 
 function playerSelection() {
@@ -9,8 +13,23 @@ function playerSelection() {
   do {
     input = prompt("Enter rock, paper or scissors").toLowerCase();
   } while (!possibleChoices.includes(input));
+  console.log("player: " + input);
   return input;
 }
 
-console.log(getComputerChoice());
-console.log(playerSelection());
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    playerScore++;
+    computerScore++;
+  } else if ((playerChoice === "rock" && computerChoice === "scissors") 
+          || (playerChoice === "paper" && computerChoice === "rock") 
+          || (playerChoice === "scissors" && computerChoice === "paper")) {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+  console.log(playerScore);
+  console.log(computerScore);
+}
+
+playRound(playerSelection(), getComputerChoice());
