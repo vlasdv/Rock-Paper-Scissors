52a20c7fa22019064f3d69bd20c5d74eb944c2f6
Dmitrii Vlasov <vlasdv@users.noreply.github.com>
2023-10-15 21:33:37 +0530

    Add function for getting user input

--- static/app.js
+++ static/app.js
@@ -1,7 +1,16 @@
-const possibleChoices = ["Rock", "Paper", "Scissors"];
+const possibleChoices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return possibleChoices[Math.floor(Math.random() * 3)];
 }
 
+function playerSelection() {
+  let input;
+  do {
+    input = prompt("Enter rock, paper or scissors").toLowerCase();
+  } while (!possibleChoices.includes(input));
+  return input;
+}
+
 console.log(getComputerChoice());
+console.log(playerSelection());
