4389dcf3ff692a2d82b3ad9f6f848dde08b77e2d
Dmitrii Vlasov <vlasdv@users.noreply.github.com>
2023-10-15 22:09:14 +0530

    Few changes to make code more readable

--- static/app.js
+++ static/app.js
@@ -1,9 +1,9 @@
-const possibleChoices = ["rock", "paper", "scissors"];
+const POSSIBLE_CHOICES = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  let computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
+  let computerChoice = POSSIBLE_CHOICES[Math.floor(Math.random() * 3)];
   console.log("computer: " + computerChoice);
   return computerChoice;
 }
@@ -12,7 +12,7 @@ function playerSelection() {
   let input;
   do {
     input = prompt("Enter rock, paper or scissors").toLowerCase();
-  } while (!possibleChoices.includes(input));
+  } while (POSSIBLE_CHOICES.includes(input));
   console.log("player: " + input);
   return input;
 }
@@ -21,9 +21,9 @@ function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
     playerScore++;
     computerScore++;
-  } else if ((playerChoice === "rock" && computerChoice === "scissors") 
-          || (playerChoice === "paper" && computerChoice === "rock") 
-          || (playerChoice === "scissors" && computerChoice === "paper")) {
+  } else if ((playerChoice === "rock" && computerChoice === "scissors") || 
+             (playerChoice === "paper" && computerChoice === "rock") || 
+             (playerChoice === "scissors" && computerChoice === "paper")) {
     playerScore++;
   } else {
     computerScore++;
